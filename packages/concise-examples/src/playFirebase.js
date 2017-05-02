a71aa8855130b606df08447a36869e6a9444c861
G. Grau <ggrau@me.com>
2017-05-02 15:05:45 +0200

    [examples] Add Firebase

---
+++ packages/concise-examples/src/playFirebase.js
@@ -0,0 +1,29 @@
+// @flow
+
+/* eslint-disable no-console */
+
+import path from 'path';
+import Concise from 'concise';
+import { input } from 'concise-yaml';
+import { output } from 'concise-firebase';
+
+const run = async () => {
+  try {
+    const concise = new Concise();
+    await concise.input(input, {
+      file: path.join(__dirname, '../../__tests__/fixtures/schema3_bb.yaml'),
+    });
+    // await concise.input(input, {
+    //   file: path.join(__dirname, '../../__tests__/fixtures/schema1.yaml'),
+    // });
+    // await concise.input(input, {
+    //   file: path.join(__dirname, '../../__tests__/fixtures/schema2.yaml'),
+    // });
+    const rules = await concise.output(output);
+    console.log(JSON.stringify(rules, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+run();
