9e0e50b52657be8622a35d4e3b2f9b28b99c5f46
G. Grau <ggrau@me.com>
2017-05-02 15:08:10 +0200

    [examples] Update Bookings example

--- packages/concise-examples/src/playBookings.js
+++ packages/concise-examples/src/playBookings.js
@@ -10,6 +10,7 @@ import { output as outputSvg } from 'concise-diagram';
 import { output as outputGraphql } from 'concise-graphql';
 import { output as outputPg } from 'concise-pg';
 import { output as outputSequelize } from 'concise-sequelize';
+import { output as outputFirebase } from 'concise-firebase';
 
 const run = async () => {
   const concise = new Concise();
@@ -30,9 +31,15 @@ const run = async () => {
   await concise.output(outputPg, {
     file: path.join(__dirname, 'playBookings.sql'),
   });
+  await concise.output(outputFirebase, {
+    file: path.join(__dirname, 'playBookings.firebase'),
+  });
   await concise.output(outputSequelize, {
     Sequelize,
-    sequelize: new Sequelize('postgres://postgres@localhost:5432/example'),
+    sequelize: new Sequelize('db', 'user', 'pwd', {
+      dialect: 'sqlite',
+      logging: null,
+    }),
   });
 };
 
